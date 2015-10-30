bdfbc578ad005474855015ffc6f442a71de7105b
bottelebot <botme22@gmail.com>
2015-10-30 14:39:40 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -223,7 +223,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {127828252},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
